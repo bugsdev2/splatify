ac6c3c06ac592c423a06234c040d121dbc0e9dbb
BugsLaptop <msherlock246@gmail.com>
2024-01-18 23:46:27 +0530

    Added vite set to react. Added boiler template which needs to be modified

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Logo from './components/Logo.jsx'
+import Login from './components/Login.jsx'
+
+export default function App() {
+  return (
+    <>
+		<Logo/>
+    </>
+  )
+}
+
+
---
+++ src/components/Login.jsx
@@ -0,0 +1,14 @@
+import "./Login.css"
+
+export default function Login(){
+	return (
+		<>
+			<div className="text-center">
+				<button className="btn btn-success">Login to your Account</button>
+			</div>
+			<div>
+				
+			</div>
+		</>
+	)
+}
